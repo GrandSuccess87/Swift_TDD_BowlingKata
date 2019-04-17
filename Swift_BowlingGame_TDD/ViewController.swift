ef2964a2188f43e8ef6270e7a3561eedd73fb2bf
Quintessa Anderson <quintessa@8thlight.com>
2019-04-17 14:18:06 -0500

    pushing working code that successfully loops through frames array and also loops through the rolls array and calculates the score correctly, test cases pass! (still have to test spare test case)

--- Swift_BowlingGame_TDD/ViewController.swift
+++ Swift_BowlingGame_TDD/ViewController.swift
@@ -28,32 +28,24 @@ class Game {
     
     func score () -> Int {
         var totalScore:Int = 0
+        var i:Int = 0
 //        let frames: [Int] = [10]
         let frames = [Int](1...10)
         frames.enumerated().forEach {print("Frames: ", $0, ":", $1)}
-//        print("Frames.enumerated.forEach \(frames)")
-
-//        frames.forEach { frame in
-//        for _ in 0..<frames.count {
-
-            for i in 0..<rolls.count {
-                totalScore += rolls[i]
-            }
+        for(index, element) in rolls.enumerated() {
+            print("Item \(index): \(element)")
+        }
+        
+//        var framesIterator:Int = 0
+        
+        for _ in 0..<frames.count {
+//        for i in 0..<rolls.count {
+                totalScore += rolls[i] + rolls[i+1]
+                i += 2
             
-            for(index, element) in rolls.enumerated() {
-                print("Item \(index): \(element)")
-            }
-
-//        }
-        //iterate by frame now, not through rolls array as before
-        //create i variable set equal to 0
-//        var i:Int = 0
-//        for var i in 0..<frames.count {
-//        frames.forEach { frame in
-//            totalScore += rolls[i] + rolls[i+1]
-//            i += 2
-//        }
-//        }
+//            }
+        }
+        print("Frames: \(rolls)")
 
         return totalScore;
     }
