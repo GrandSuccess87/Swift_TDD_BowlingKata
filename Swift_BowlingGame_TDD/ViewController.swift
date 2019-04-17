5e98c967ace112cb423ba1bc26f8a1f1542ed1de
Quintessa Anderson <quintessa@8thlight.com>
2019-04-17 12:50:36 -0500

    adding a for loop for frames to add the score

--- Swift_BowlingGame_TDD/ViewController.swift
+++ Swift_BowlingGame_TDD/ViewController.swift
@@ -30,19 +30,24 @@ class Game {
         var totalScore:Int = 0
         let frames: [Int] = [10]
         
-        frames.forEach { frame in
+//        frames.forEach { frame in
         
+        //print current index and it's value
         for(index, element) in rolls.enumerated() {
             print("Item \(index): \(element)")
         }
-            for i in 0..<rolls.count {
-                totalScore += rolls[i]
-            }
-//            rolls.forEach { roll in
-//                totalScore += roll
-            
-//            }
+        
+        //iterate by frame now, not through rolls array as before
+        //create i variable set equal to 0
+//        var i:Int = 0
+        for i in 0..<frames.count {
+            totalScore += rolls[i] + rolls[i+1]
+            i += 2
         }
+//            for i in 0..<rolls.count {
+//                totalScore += rolls[i]
+//            }
+//        }
     
         return totalScore;
     }
