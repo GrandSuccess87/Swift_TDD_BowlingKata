727c9a9a7d0a5661f59694b73191ba8d99ee06c2
Quintessa Anderson <quintessa@8thlight.com>
2019-04-17 13:05:55 -0500

    test cases passes, all but testOneSpare

--- Swift_BowlingGame_TDD/ViewController.swift
+++ Swift_BowlingGame_TDD/ViewController.swift
@@ -29,26 +29,26 @@ class Game {
     func score () -> Int {
         var totalScore:Int = 0
         let frames: [Int] = [10]
-        
-//        frames.forEach { frame in
-        
-        //print current index and it's value
-        for(index, element) in rolls.enumerated() {
-            print("Item \(index): \(element)")
+        frames.forEach { frame in
+            
+            for(index, element) in rolls.enumerated() {
+                print("Item \(index): \(element)")
+            }
+            
+            for i in 0..<rolls.count {
+                totalScore += rolls[i]
+            }
         }
-        
         //iterate by frame now, not through rolls array as before
         //create i variable set equal to 0
 //        var i:Int = 0
-        for i in 0..<frames.count {
-            totalScore += rolls[i] + rolls[i+1]
-            i += 2
-        }
-//            for i in 0..<rolls.count {
-//                totalScore += rolls[i]
-//            }
+//        for var i in 0..<frames.count {
+//        frames.forEach { frame in
+//            totalScore += rolls[i] + rolls[i+1]
+//            i += 2
 //        }
-    
+//        }
+
         return totalScore;
     }
 }
