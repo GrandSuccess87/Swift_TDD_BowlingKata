5a34092854097a0828fbb3bd241e1f62645ae17c
Quintessa Anderson <quintessa@8thlight.com>
2019-04-17 13:37:03 -0500

    figured out how to find the index for a for each loop, test cases pass except spare

--- Swift_BowlingGame_TDD/ViewController.swift
+++ Swift_BowlingGame_TDD/ViewController.swift
@@ -28,17 +28,23 @@ class Game {
     
     func score () -> Int {
         var totalScore:Int = 0
-        let frames: [Int] = [10]
-        frames.forEach { frame in
+//        let frames: [Int] = [10]
+        let frames = [Int](1...10)
+        frames.enumerated().forEach {print("Frames: ", $0, ":", $1)}
+//        print("Frames.enumerated.forEach \(frames)")
+
+//        frames.forEach { frame in
+//        for _ in 0..<frames.count {
+
+            for i in 0..<rolls.count {
+                totalScore += rolls[i]
+            }
             
             for(index, element) in rolls.enumerated() {
                 print("Item \(index): \(element)")
             }
-            
-            for i in 0..<rolls.count {
-                totalScore += rolls[i]
-            }
-        }
+
+//        }
         //iterate by frame now, not through rolls array as before
         //create i variable set equal to 0
 //        var i:Int = 0
